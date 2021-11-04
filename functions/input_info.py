ed291af22d03946e841fbd39962cfdd8382866ec
rogierr <rogierhar@hotmail.com>
2021-11-04 22:57:21 +0100

    Input info is now type hinted, functions are rewritten according to PEP style.

--- functions/input_info.py
+++ functions/input_info.py
@@ -8,20 +8,38 @@ sys.path.insert(0, sysdir)
 sys.path.insert(0, os.path.dirname(__file__) + 'shared_memory')
 os.environ['PATH'] = os.environ['PATH'] + ";."
 
-from functions.shared_memory import info
 
-# Between 0 and 1
-def getGasInput(car=0):
+def get_gas_input(car: int = 0) -> float:
+    """
+    Retrieve the gas input given to a car
+    :param car: car in the game (0 is user)
+    :return: gas input between 0 and 1
+    """
     return ac.getCarState(car, acsys.CS.Gas)
 
-# Between 0 and 1
-def getBrakeInput(car=0):
+
+def get_brake_input(car: int = 0) -> float:
+    """
+    Retrieve the brake input given to a car
+    :param car: car in the game (0 is user)
+    :return: brake input between 0 and 1
+    """
     return ac.getCarState(car, acsys.CS.Brake)
 
-# Not based on input, but is based on whether the clutch is 'biting' in the car or no. Between 0 and 1
-def getClutch(car=0):
+
+def get_clutch(car: int = 0) -> float:
+    """
+    Retrieve the clutch status in the game of a car
+    :param car: car in the game (0 is user)
+    :return: deployment of the clutch (1 is fully deployed, 0 is not deployed).
+    """
     return ac.getCarState(car, acsys.CS.Clutch)
 
-# In radians [-2pi, 2pi]
-def getSteerInput(car=0):
+
+def get_steer_input(car: int = 0) -> float:
+    """
+    Retrieve the steering input given to a car
+    :param car: car in the game (0 is user)
+    :return: steering input to the car, depends on the settings in AC, in degrees
+    """
     return ac.getCarState(car, acsys.CS.Steer)
