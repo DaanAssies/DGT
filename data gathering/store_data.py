f9c4f03dace2094e3ea0c7d86c53801fd1c3c6ba
rogierr <rogierhar@hotmail.com>
2021-12-14 23:35:56 +0100

    Rewriting array capture functions, next to that added multiple other array functions.

--- data gathering/store_data.py
+++ data gathering/store_data.py
@@ -1,14 +1,13 @@
 import functions.tyre_info
 import functions.input_info
 import functions.session_info
-# import functions.lap_info
-# import functions.car_info
-# import functions.car_stats
-# import functions.aero_info
+import functions.lap_info
+import functions.car_info
+import functions.car_stats
+import functions.aero_info
 from datetime import datetime
 
 import numpy as np
-
 
 
 class DataCollector:
@@ -36,10 +35,10 @@ class DataCollector:
             tyre_array = self.capture_tyre_info()
 
         if self._capture_input_info:
-            print("Capture input info here")
+            input_array = self.capture_input_info()
 
         if self._capture_session_info:
-            print("Capture session info here")
+            session_array = self.capture_session_info()
 
         if self._capture_lap_info:
             print("Capture lap info here")
@@ -48,10 +47,10 @@ class DataCollector:
             print("Capture car info")
 
         if self._capture_car_stats:
-            print("Capture car stats")
+            car_stats_array = self.capture_car_stats()
 
         if self._capture_aero_info:
-            print("Capture aero info")
+            print("placeholder, aero data currently bugged")
 
     @staticmethod
     def capture_tyre_info():
@@ -95,44 +94,48 @@ class DataCollector:
         brake_temp_rl = functions.tyre_info.get_brake_temp(2)
         brake_temp_rr = functions.tyre_info.get_brake_temp(3)
 
-        tyre_wear_array = np.array([('Tyre Wear Front Left', tyre_wear_fl), ('Tyre Wear Front Right', tyre_wear_fr),
-                                    ('Tyre Wear Rear Left', tyre_wear_rl), ('Tyre Wear Rear Right', tyre_wear_rr)],
-                                   dtype=[('Variable name', 'string'), ('value', 'float')])
+        tyre_wear_array = np.array([(tyre_wear_fl, tyre_wear_fr, tyre_wear_rl, tyre_wear_rr)],
+                                   dtype=[('Tyre Wear Front Left', 'float'), ('Tyre Wear Front Right', 'float'),
+                                          ('Tyre Wear Rear Left', 'float'), ('Tyre Waer Rear Right', 'float')])
 
-        tyre_dirt_array = np.array([('Tyre Dirt Front Left', tyre_dirt_fl), ('Tyre Dirt Front Right', tyre_dirt_fr),
-                                    ('Tyre Dirt Rear Left', tyre_dirt_rl), ('Tyre Dirt Rear Right', tyre_dirt_rr)],
-                                   dtype=[('Variable name', 'string'), ('value', 'float')])
+        tyre_dirt_array = np.array([(tyre_dirt_fl, tyre_dirt_fr, tyre_dirt_rl, tyre_dirt_rr)],
+                                   dtype=[('Tyre Dirt Front Left', 'float'), ('Tyre Dirt Front Right', 'float'),
+                                          ('Tyre Dirt Rear Left', 'float'), ('Tyre Dirt Rear Right', 'float')])
 
-        tyre_temp_array = np.array([('Tyre Temp Front Left Inner', tyre_temp_fl_inner),
-                                    ('Tyre Temp Front Left Middle', tyre_temp_fl_middle),
-                                    ('Tyre Temp Front Left Outer', tyre_temp_fl_outer),
-                                    ('Tyre Temp Front Left Core', tyre_temp_fl_core),
-                                    ('Tyre Temp Front Right Inner', tyre_temp_fr_inner),
-                                    ('Tyre Temp Front Right Middle', tyre_temp_fr_middle),
-                                    ('Tyre Temp Front Right Outer', tyre_temp_fr_outer),
-                                    ('Tyre Temp Front Right Core', tyre_temp_fr_core),
-                                    ('Tyre Temp Rear Left Inner', tyre_temp_rl_inner),
-                                    ('Tyre Temp Rear Left Middle', tyre_temp_rl_middle),
-                                    ('Tyre Temp Rear Left Outer', tyre_temp_rl_outer),
-                                    ('Tyre Temp Rear Left Core', tyre_temp_rl_core),
-                                    ('Tyre Temp Rear Right Inner', tyre_temp_rr_inner),
-                                    ('Tyre Temp Rear Right Middle', tyre_temp_rr_middle),
-                                    ('Tyre Temp Rear Right Outer', tyre_temp_rr_outer),
-                                    ('Tyre Temp Rear Right Core', tyre_temp_rr_core)
+        tyre_temp_array = np.array([(tyre_temp_fl_inner, tyre_temp_fl_middle, tyre_temp_fl_outer, tyre_temp_fl_core,
+                                     tyre_temp_fr_inner, tyre_temp_fr_middle, tyre_temp_fr_outer, tyre_temp_fr_core,
+                                     tyre_temp_rl_inner, tyre_temp_rl_middle, tyre_temp_rl_outer, tyre_temp_rl_core,
+                                     tyre_temp_rr_inner, tyre_temp_rr_middle, tyre_temp_rr_outer, tyre_temp_rr_core)
                                     ],
-                                   dtype=[('Variable name', 'string'), ('value', 'float')])
+                                   dtype=[('Tyre Temp Front Left Inner', 'float'),
+                                          ('Tyre Temp Front Left Middle', 'float'),
+                                          ('Tyre Temp Front Left Outer', 'float'),
+                                          ('Tyre Temp Front Left Core', 'float'),
+                                          ('Tyre Temp Front Right Inner', 'float'),
+                                          ('Tyre Temp Front Right Middle', 'float'),
+                                          ('Tyre Temp Front Right Outer', 'float'),
+                                          ('Tyre Temp Front Right Core', 'float'),
+                                          ('Tyre Temp Rear Left Inner', 'float'),
+                                          ('Tyre Temp Rear Left Middle', 'float'),
+                                          ('Tyre Temp Rear Left Outer', 'float'),
+                                          ('Tyre Temp Rear Left Core', 'float'),
+                                          ('Tyre Temp Rear Right Inner', 'float'),
+                                          ('Tyre Temp Rear Right Middle', 'float'),
+                                          ('Tyre Temp Rear Right Outer', 'float'),
+                                          ('Tyre Temp Rear Right Core', 'float')
+                                          ])
 
-        tyre_pressure_array = np.array([('Tyre Pressure Front Left', tyre_pressure_fl),
-                                        ('Tyre Pressure Front Right', tyre_pressure_fr),
-                                        ('Tyre Pressure Rear Left', tyre_pressure_rl),
-                                        ('Tyre Pressure Rear Right', tyre_pressure_rr)],
-                                       dtype=[('Variable name', 'string'), ('value', 'float')])
+        tyre_pressure_array = np.array([(tyre_pressure_fl, tyre_pressure_fr, tyre_pressure_rl, tyre_pressure_rr)],
+                                       dtype=[('Tyre Pressure Front Left', 'float'),
+                                              ('Tyre Pressure Front Right', 'float'),
+                                              ('Tyre Pressure Rear Left', 'float'),
+                                              ('Tyre Pressure Rear Right', 'float')])
 
-        brake_temperature_array = np.array([('Brake Temperature Front Left', brake_temp_fl),
-                                            ('Brake Temperature Front Right', brake_temp_fr),
-                                            ('Brake Temperature Rear Left', brake_temp_rl),
-                                            ('Brake Temp Rear Right', brake_temp_rr)],
-                                           dtype=[('Variable name', 'string'), ('value', 'float')])
+        brake_temperature_array = np.array([(brake_temp_fl, brake_temp_fr, brake_temp_rl, brake_temp_rr)],
+                                           dtype=[('Break Temperature Front Left', 'float'),
+                                                  ('Break Temperature Front Right', 'float'),
+                                                  ('Break Temperature Rear Left', 'float'),
+                                                  ('Break Temperature Rear Right', 'float')])
 
         tyre_array = np.hstack(tyre_wear_array, tyre_dirt_array, tyre_temp_array, tyre_pressure_array,
                                brake_temperature_array)
@@ -146,8 +149,9 @@ class DataCollector:
         clutch_input = functions.input_info.get_clutch()
         steer_input = functions.input_info.get_steer_input()
 
-        input_array = np.array([('Gas Input', gas_input), ('Brake Input', brake_input), ('Clutch Input', clutch_input),
-                                ('Steer Input', steer_input)], dtype=[('Variable name', 'string'), ('value', 'float')])
+        input_array = np.array([(gas_input, brake_input, clutch_input, steer_input)],
+                               dtype=[('Gas Input', 'float'), ('Brake Input', 'float'),
+                                      ('Clutch Input', 'float'), ('Steer Input', 'float')])
 
         return input_array
 
@@ -162,6 +166,18 @@ class DataCollector:
         cars_count = functions.session_info.get_cars_count()
         session_status = functions.session_info.get_session_status()
 
+        session_type_array = np.array([session_type], dtype=['Session Type'])
+        string_info_array = np.array([(driver_name, car_name, track_name, track_config)],
+                                     dtype=[('Driver Name', 'string'), ('Car Name', 'string'),
+                                            ('Track Name', 'string'), ('Track Config', 'string')])
+        float_info_array = np.array([track_length], dtype=[('Track length', 'float')])
+        int_info_array = np.array([(cars_count, session_status)],
+                                  dtype=[('Cars Count', 'int'), ('Session Status', 'int')])
+
+        session_array = np.hstack(session_type_array, string_info_array, float_info_array, int_info_array)
+
+        return session_array
+
     @staticmethod
     def capture_lap_info():
         print("Lap information")
@@ -172,11 +188,22 @@ class DataCollector:
 
     @staticmethod
     def capture_car_stats():
-        print("Car stats")
+        has_drs = functions.car_stats.get_has_drs()
+        has_ers = functions.car_stats.get_has_ers()
+        has_kers = functions.car_stats.get_has_kers()
+        abs_level = functions.car_stats.abs_level()
+        max_rpm = functions.car_stats.get_max_rpm()
+        max_fuel = functions.car_stats.get_max_fuel()
+
+        car_stats_array = np.array([(has_drs, has_ers, has_kers, abs_level, max_rpm, max_fuel)],
+                                   dtype=[('Car Has DRS', 'int'), ('Car Has ERS', 'int'), ('Car Has KERS', 'int'),
+                                            ('Car ABS Level', 'int'), ('Car Max RPM', 'int'), ('Car Max Fuel', 'int')])
+
+        return car_stats_array
 
     @staticmethod
     def capture_aero_info():
-        print("Aero info")
+        print("Aero info placeholder")
 
 
 test = DataCollector()
